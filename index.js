395296fad56dfcf17db582c855c2b61d115c5762
Boogey Man <maiphp@gmail.com>
2021-07-30 14:29:12 +0700

    update code from example

--- index.js
+++ index.js
@@ -1,59 +1,51 @@
-// Exampole Echo reply
+'use strict';
 
-const express = require('express')
-// const bodyParser = require('body-parser')
-const axios = require('axios')
-const app = express()
-const line = require('@line/bot-sdk')
-require('dotenv').config()
+const line = require('@line/bot-sdk');
+const express = require('express');
 
-const port = process.env.PORT || 8080
-const host = '0.0.0.0'
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
 
-const client = new line.Client({
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+// listen on port
+const port = process.env.PORT || 3000;
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
 });
 
-// Body parser
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-
-// Start Routing section
-app.get('/', (req, res) => {
-	res.send({'status':'OK'})
-})
-
-// recieve request from Line
-app.post('/webhook', (req, res) => {
-    //console.log(req.body);
-    let reqBody = req.body;
-
-    let msg = `\n`+
-    `Token = ${process.env.CHANNEL_ACCESS_TOKEN}\n` +
-    `Port = ${port}\n`
-    console.log(msg)
-
-    if(reqBody.events[0] !== undefined ) {
-        let replyToken = reqBody.events[0].replyToken
-        let msg = reqBody.events[0].message.text
-
-        client.replyMessage(replyToken, msg)
-            .then((res) => {
-                console.log(res)
-            })
-            .catch((err) => {
-                // error handling
-                console.log(err)
-            });
-    }else{
-        console.log(`Webhook verified.`)
-        res.sendStatus(200)
+// event handler
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        // ignore non-text-message event
+        return Promise.resolve(null);
     }
     
-})
-// End Routing section
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
 
-// Listening
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+}
+
 app.listen(port, () => {
-    console.log(`Listening at ${host} on port ${port}`);
+    console.log(`listening on ${port}`);
 });
